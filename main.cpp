d5edb618dd6a06466d764dca6d835d6fecedc4c2
xatomen <jorgito899@gmail.com>
2024-06-08 23:46:42 -0400

    Avance en lectura

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
+#include <omp.h>
 
 int main() {
     std::ifstream archivo("/home/jorge/Escritorio/Proyectos/Datos/pd.csv");
@@ -13,45 +14,50 @@ int main() {
     char delimitador = ';';
     getline(archivo, linea);
 
-    //std::vector<Producto> Productos;
+    std::vector<Producto> Productos;
     clock_t t;
     t = clock();
-
-    while(getline(archivo,linea)){
+    long i = 0;
+    while (getline(archivo, linea)) {
         //std::cout << linea << std::endl;
-        /*
-        std::vector<std::string> fila = csv.GetRow<std::string> (i);
+        std::stringstream stream(linea);
+        std::string created;
+        getline(stream, created, delimitador);  //Obtener la fecha (created) de la transacción
+        std::cout << "1" << std::endl;
+        std::cout << created << std::endl;
+        int anho = stoi(created.substr(1,4)); //Extraer el año del string created
+        int mes = stoi(created.substr(6,2));  //Extraer el mes del string created
+        int dia = stoi(created.substr(9,2));  //Extraer el día del string created
+        std::cout << "2" << std::endl;
 
-        std::string created = fila[0];  //Obtener la fecha (created) de la transacción
+        std::string saltar;
+        getline(stream,saltar,delimitador);
+        getline(stream,saltar,delimitador);
+        getline(stream,saltar,delimitador);
+        getline(stream,saltar,delimitador);
+        getline(stream,saltar,delimitador);
+        std::string sku;
+        getline(stream,sku,delimitador);
+        getline(stream,saltar,delimitador);
+        getline(stream,saltar,delimitador);
+        std::string str_amount;
+        getline(stream,str_amount,delimitador);
 
-        int anho = stoi(created.substr(0,4)); //Extraer el año del string created
-        int mes = stoi(created.substr(5,2));  //Extraer el mes del string created
-        int dia = stoi(created.substr(8,2));  //Extraer el día del string created
-
-        int payment = fila[1].empty() ? 0 : std::stoi(fila[1]);  //Convertir número de boleta de string a int, si está vacío, asignar 0, en caso contrario, realizar la conversión.
-        int store_fk = fila[2].empty() ? 0 : std::stoi(fila[2]);  //Convertir número de tienda de string a int, si está vacío, asignar 0, en caso contrario, realizar la con
-        std::string seller = fila[3];  //Extraer el
-        std::string category = fila[4];
-        std::string delivery_type = fila[5];
-        std::string sku = fila[6];
-        int quantity = fila[7].empty() ? 0 : std::stoi(fila[7]);
-        std::string name = fila[8];
-        float amount = fila[9].empty() ? 0 : std::stof(fila[9]);
+        float amount = stof(str_amount);
 
         Producto producto;
         producto.setCreated(anho, mes, dia);
-        producto.setPayment(payment);
-        producto.setStore_fk(store_fk);
-        producto.setSeller(seller);
-        producto.setCategory(category);
-        producto.setDelivery_type(delivery_type);
+        //producto.setPayment(payment);
+        //producto.setStore_fk(store_fk);
+        //producto.setSeller(seller);
+        //producto.setCategory(category);
+        //producto.setDelivery_type(delivery_type);
         producto.setSku(sku);
-        producto.setQuantity(quantity);
-        producto.setName(name);
+        //producto.setQuantity(quantity);
+        //producto.setName(name);
         producto.setAmount(amount);
-
         Productos[i] = producto;
-        */
+        i++;
     }
     t = clock() - t;
     std::cout << "clock t = " << t << std::endl;
